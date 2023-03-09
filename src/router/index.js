806bbe00a5ccb2d7cc87e01db056e9790eed0fda
kiran <ch.kirankumar311@gmail.cm>
2023-03-09 18:10:08 +0530

    nova99_singlePage_signUp

--- src/router/index.js
+++ src/router/index.js
@@ -8,31 +8,9 @@ const router = createRouter({
       path: '/signup/phone',
       name: 'signupPhone',
       component: () => import('@/views/SignUpPhoneView.vue')
-    },
-    {
-      path: '/signup/mobile',
-      name: 'signupMobile',
-      component: () => import('@/views/SignUpMobileView.vue')
-    },
-    {
-      path: '/signup/mobile/otp',
-      name: 'signupMobileOtp',
-      component: () => import('@/views/SignUpMobileOtpView.vue')
-    },
-    {
-      path: '/signup/document',
-      name: 'signupDocument',
-      component: () => import('@/views/DocumentUploadPage.vue')
-    },
-
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    }
+    
+   
   ]
 })
 
